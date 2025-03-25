ebe8a09d6a85acfe87731e75026196403bffa13b
Braian Lopez <braiilopez16@gmail.com>
2025-03-25 16:52:00 -0300

    no message

---
+++ Producto.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Musimundo
+{
+    internal class Producto
+    {
+        public string Descripcion;
+
+        private int Stock;
+
+        //PROPIEDADES ESTATICAS
+        public static int TotalProductos { get; private set; } = 0;
+        public static List<Producto> TodosLosProductos = new List<Producto>();
+
+
+        //GETTER Y SETTER PARA STOCK
+        public int GetStock() { return Stock; }
+        public void SetStock(int stock)
+        {
+            if (stock >= 0)
+
+                Stock = stock;
+
+            else
+
+                throw new ArgumentException("el stock no puede ser negativo");
+        }
+        //CONSTRUCTOR
+        public Producto(string descripcion, int stock)
+        {
+            Descripcion = descripcion;
+            SetStock(stock);
+            TotalProductos += stock;
+        }
+
+        public static void OrdenarPorStock()
+        {
+            int indiceProducto = 0;
+            Console.WriteLine("Resumen del stock del inventario: \n");
+            foreach (Producto producto in TodosLosProductos.OrderBy(producto => producto.GetStock()).ToList())
+            {
+                indiceProducto++;
+                Console.WriteLine($"{indiceProducto} {producto.Descripcion} - stock actual:  {producto.GetStock()}");
+            }
+        }
+        public static void AgregarProducto(Producto productoNuevo)
+        {
+            TodosLosProductos.Add(productoNuevo);
+        }
+
+        public static void MostrarProductos()
+
+        {
+            Console.WriteLine("PRODUCTOS(" + TodosLosProductos.Count() + ")\n");
+            if (TotalProductos > 0)
+            {
+                int indiceProducto = 0;
+                foreach (Producto producto in TodosLosProductos.OrderBy(producto => producto.Descripcion).ToList())
+                {
+                    indiceProducto++;
+                    Console.WriteLine($"{indiceProducto} {producto.Descripcion} - stock actual:  {producto.GetStock()}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Todavia no hay productos cargados");
+            }
+            Console.WriteLine("\n");
+
+        }
+        public static void ProductoConMayorStock()
+        {
+            Producto pConMasStock = TodosLosProductos[0];
+            foreach (Producto producto in TodosLosProductos)
+            {
+                if (producto.GetStock() > pConMasStock.GetStock())
+                {
+                    pConMasStock = producto;
+                }
+            }
+            Console.WriteLine($"Producto con mayor stock: {pConMasStock.Descripcion} - stock: {pConMasStock.GetStock()}");
+        }
+        public static void ProductoConMenorStock()
+        {
+            Producto pConMenosStock = TodosLosProductos[0];
+            foreach (Producto producto in TodosLosProductos)
+            {
+                if (producto.GetStock() < pConMenosStock.GetStock())
+                {
+                    pConMenosStock = producto;
+                }
+            }
+            Console.WriteLine($"Producto con menor stock: {pConMenosStock.Descripcion} - stock: {pConMenosStock.GetStock()}");
+        }
+
+        public static void GenerarReporte()
+        {
+            Console.WriteLine($"Total de productos en inventario {TotalProductos}\n");
+            ProductoConMayorStock();
+            ProductoConMenorStock();
+            OrdenarPorStock();
+        }
+
+        public static void EliminarProducto(int productoAEliminar)
+        {
+            if (TodosLosProductos.Count() >= productoAEliminar)
+            {
+                Console.Clear();
+                Console.WriteLine("¿Seguro que querés eliminar " + TodosLosProductos[productoAEliminar - 1].Descripcion + "?\n  1=si/2=no\n");
+                int confirmaEliminar = int.Parse(Console.ReadLine());
+                if (confirmaEliminar == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Producto " + TodosLosProductos[productoAEliminar - 1].Descripcion + " eliminado con exito.\n");
+                    TotalProductos -= TodosLosProductos[productoAEliminar - 1].GetStock();
+                    TodosLosProductos.Remove(TodosLosProductos[productoAEliminar - 1]);
+                }
+                else if (confirmaEliminar == 2)
+                {
+                    Console.WriteLine("proceso 'eliminar' cancelado.\n");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("El producto que intentas eliminar no existe.\n");
+            }
+        }
+
+
+        public void EditarProducto()
+        {
+            Console.Clear();
+            Console.WriteLine("la opcion elegida es: " + Descripcion);
+            Console.WriteLine("ingresá el nuevo nombre del producto\n");
+            Descripcion = Console.ReadLine();
+            Console.WriteLine("Stock inicial: " + GetStock());
+            Console.WriteLine("ingresá la nueva cantidad de stock del producto\n");
+            SetStock(int.Parse(Console.ReadLine()));
+            if (Descripcion != "")
+            {
+                Console.Clear();
+                Console.WriteLine("Producto editado.\n");
+                return;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("ingrese un  nombre valido para el producto. intente nuevamente");
+            }
+        }
+        public static void Buscar()
+        {
+            Console.Clear();
+            Console.WriteLine("ingresá el producto que querés buscar");
+            var productoBuscado = Console.ReadLine();
+            var productoEncontrado = TodosLosProductos.Find(producto => producto.Descripcion.Contains(productoBuscado));
+            if (productoEncontrado == null)
+            {
+                Console.WriteLine("No se encontró este producto. Intentá buscando otro o agregalo al sistema.");
+            }
+            else
+            {
+                Console.WriteLine($" {productoEncontrado.Descripcion} - stock actual:  {productoEncontrado.GetStock()}");
+            }
+        }
+
+        public static void CargarArchivo()
+        {
+            TextWriter archivo = new StreamWriter("C:\\Users\\braia\\source\\repos\\Musimundo\\datos-inventario-musimundo.TXT");
+            TodosLosProductos.ForEach(producto => archivo.WriteLine(producto.Descripcion + ";" + producto.GetStock()));
+            archivo.Close();
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musimundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Producto producto1 = new Producto("play 3", 2);
+            //Producto producto2 = new Producto("play 4", 1);
+            //Producto producto3 = new Producto("play 5", 4);
+            //Producto.AgregarProducto(producto1);
+            //Producto.AgregarProducto(producto2);
+            //Producto.AgregarProducto(producto3);
+            try { 
+            string rutaDelArchivo = "C:\\Users\\braia\\source\\repos\\Musimundo\\datos-inventario-musimundo.TXT";
+            using(StreamReader sr = new StreamReader(rutaDelArchivo))
+            {
+                string linea;
+                while((linea = sr.ReadLine()) != null)
+                {
+                    string[]  productoDelArchivo = linea.Split(';');
+                    Producto producto = new Producto(productoDelArchivo[0], int.Parse(productoDelArchivo[1]));
+                    Producto.AgregarProducto(producto);
+
+                }
+            }
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Revisá la ruta del archivo:\n "+ex.Message);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("No se encontró el archivo:\n " + ex.Message);
+            }
+
+
+
+            //Variables
+            string mostrarMenu = "\nEscribí el numero de la opcion que deseas realizar:\n1- Ver Productos\n2- Agregar un producto \n3- Eliminar un producto\n4- Editar un producto\n5- Buscar\n6- Reportes y estadísticas \n0- Salir \n";
+            int opcion;
+            string descripcion;
+
+            //Mensaje de bienvenida
+            Console.WriteLine("Hola, bienvenido a  Musimundo!\n");
+
+            //Bucle do-while para elegir la opcion a realizar(agregar producto, ver la lista de productos, etc)
+            do
+            {
+                opcion = leerOpcion();
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Clear();
+                        Producto.MostrarProductos();
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Ingrese descripción del producto: \n");
+                        descripcion = Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine("\nIngrese stock inicial del producto: \n");
+                        if (int.TryParse(Console.ReadLine(), out int stock))
+                        {
+
+                        }
+                        Console.Clear();
+                        Producto productoNuevo = new Producto(descripcion, stock);
+                        Producto.AgregarProducto(productoNuevo);
+
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        Producto.MostrarProductos();
+
+                        Console.WriteLine("Escribi el numero del producto que querés eliminar: \n");
+                        if(int.TryParse(Console.ReadLine(), out int productoAEliminar)){
+
+                        Producto.EliminarProducto(productoAEliminar);
+                        }
+
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Producto.MostrarProductos();
+                        Console.WriteLine("0.Cancelar\n");
+                        Console.WriteLine("ingresá el numero de el producto que deseas editar:\n");
+                        int prodAEditar = int.Parse(Console.ReadLine());
+                        if (prodAEditar == 0)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("operacion cancelada");
+                        }
+                        else
+                        {
+                            Producto productoAEditar = Producto.TodosLosProductos[prodAEditar-1];
+                            productoAEditar.EditarProducto();
+                        }
+                        break;
+                    case 5: 
+                        Console.Clear();
+                        Producto.Buscar();
+                        break;
+                    case 6:
+                        Console.Clear();
+                        Producto.GenerarReporte();
+                        break;
+
+                }
+
+            } while (opcion != 0);
+            
+            
+            Producto.CargarArchivo();
+            
+
+
+            //Metodos
+
+
+            int leerOpcion()
+            {
+                int numeroOpcion;
+                while (true)
+                {
+                    Console.Write(mostrarMenu);
+                    if (int.TryParse(Console.ReadLine(), out numeroOpcion))
+                    {
+                        if (numeroOpcion > 6)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("por favor, ingresa una opcion valida.");
+                        }
+                        return numeroOpcion;
+                    }
+                    Console.Clear();
+                    Console.WriteLine("Por favor ingresa una opcion válida.");
+                }
+            }
+        }   
+    }
+}
